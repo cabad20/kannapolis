f448185a3f62cc35d80f8a9c57df087195ec520a
christian abad <cabad20@gmail.com>
2016-07-12 15:14:27 -0400

    changes to the mobile view

--- sources.js
+++ sources.js
@@ -2,10 +2,10 @@ var options = [
   {selector: '.aboutInfo', offset: 50, callback: function() {
     Materialize.fadeInImage('.aboutInfo');
   } },
-  {selector: '.offer', offset: 205, callback: function() {
+  {selector: '.rollOut', offset: 205, callback: function() {
     Materialize.toast("We ONLY offer group tours of 25 people or more!", 500 );
   } },
-  {selector: '.otherInfo', offset: 400, callback: function() {
+  {selector: '.otherInfo', offset: 60, callback: function() {
     Materialize.showStaggeredList(".otherInfo");
   } },
   {selector: '.contactInfo', offset: 30, callback: function() {
