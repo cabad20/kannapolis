c75586790e9536b6d6d4a2043ca08f9120041906
christian abad <cabad20@gmail.com>
2016-07-12 14:45:52 -0400

    implemeting materialize

--- sources.js
+++ sources.js
@@ -1,15 +1,15 @@
 var options = [
-  {selector: '.mainSection', offset: 50, callback: function() {
-    Materialize.fadeInImage('.mainSection');
+  {selector: '.aboutInfo', offset: 50, callback: function() {
+    Materialize.fadeInImage('.aboutInfo');
   } },
   {selector: '.offer', offset: 205, callback: function() {
     Materialize.toast("We ONLY offer group tours of 25 people or more!", 500 );
   } },
-  // {selector: '#staggered-test', offset: 400, callback: function() {
-  //   Materialize.showStaggeredList("#staggered-test");
-  // } },
-  {selector: '.footerPortion', offset: 50, callback: function() {
-    Materialize.fadeInImage('.footerPortion');
+  {selector: '.otherInfo', offset: 400, callback: function() {
+    Materialize.showStaggeredList(".otherInfo");
+  } },
+  {selector: '.contactInfo', offset: 30, callback: function() {
+    Materialize.fadeInImage('.contactInfo');
   } }
 ];
 Materialize.scrollFire(options);
