3b103e1391ba899e3b120128e81d2b750d574b33
christian abad <cabad20@gmail.com>
2016-07-19 09:29:02 -0400

    changes to the layout

--- sources.js
+++ sources.js
@@ -1,14 +1,11 @@
 var options = [
-  {selector: '.aboutInfo', offset: 50, callback: function() {
-    Materialize.fadeInImage('.aboutInfo');
+  {selector: '.rollOut', offset: 50, callback: function() {
+    Materialize.toast("We ONLY offer group tours of 25 people or more!", 1500 );
   } },
-  {selector: '.rollOut', offset: 205, callback: function() {
-    Materialize.toast("We ONLY offer group tours of 25 people or more!", 500 );
-  } },
-  {selector: '.otherInfo', offset: 60, callback: function() {
+  {selector: '.otherInfo', offset: 200, callback: function() {
     Materialize.showStaggeredList(".otherInfo");
   } },
-  {selector: '.contactInfo', offset: 30, callback: function() {
+  {selector: '.contactInfo', offset: 205, callback: function() {
     Materialize.fadeInImage('.contactInfo');
   } }
 ];
